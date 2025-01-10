b4b9756aec8863d36d8d0ff3d1c8073dde4b44fb
han6man <han6man@gmail.com>
2025-01-10 11:05:06 +0200

    Tree Generation #2

--- UnityProject/Survival_Game_Unity/Assets/TerrainGeneration.cs
+++ UnityProject/Survival_Game_Unity/Assets/TerrainGeneration.cs
@@ -1,19 +1,36 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    public Sprite tile;
+    [Header("Tile Sprites")]
+    [SerializeField] private Sprite grass;
+    [SerializeField] private Sprite dirt;
+    [SerializeField] private Sprite stone;
+    [SerializeField] private Sprite log;
+    [SerializeField] private Sprite leaf;
 
-    public float surfaceValue = 0.25f;
-    public int worldSize = 100;
-    public float caveFreq = 0.05f;
-    public float terrainFreq = 0.05f;
-    public float heightMultiplier = 4.0f;
-    public int heightAddition = 25;
+    [Header("Trees")]
+    [SerializeField] private int treeChance = 15;
+    [SerializeField] private int minTreeHeight = 3;
+    [SerializeField] private int maxTreeHeight = 6;
 
-    public float seed;
-    public Texture2D noiseTexture;
+    [Header("Generation Settings")]
+    [SerializeField] private bool generateCaves = true;
+    [SerializeField] private int dirtLayerHeight = 5;
+    [SerializeField] private float surfaceValue = 0.25f;
+    [SerializeField] private int worldSize = 100;
+    [SerializeField] private float heightMultiplier = 25.0f;
+    [SerializeField] private int heightAddition = 25;
+
+    [Header("Noise Settings")]
+    [SerializeField] private float terrainFreq = 0.04f;
+    [SerializeField] private float caveFreq = 0.08f;
+    [SerializeField] private float seed;
+    [SerializeField] private Texture2D noiseTexture;
+
+    private List<Vector2> worldTiles = new List<Vector2>();
 
     private void Start()
     {
@@ -22,21 +39,52 @@ public class TerrainGeneration : MonoBehaviour
         GenerateTerrain();
     }
 
-    public void GenerateTerrain()
+    private void GenerateTerrain()
     {
         for (int x = 0; x < worldSize; x++)
         {
-            float heiht = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
 
-            for (int y = 0; y < heiht; y++)
+            for (int y = 0; y < height; y++)
             {
-                if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                Sprite tileSprite;
+                if (y < height - dirtLayerHeight)
                 {
-                    GameObject newTile = new GameObject(name = "tile");
-                    newTile.transform.parent = this.transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    tileSprite = stone;
+                }
+                else if (y < height - 1)
+                {
+                    tileSprite = dirt;
+                }
+                else
+                {
+                    // top layer of the terrain
+                    tileSprite = grass;
+                }
+
+                if (generateCaves)
+                {
+                    if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                    {
+                        PlaceTile(tileSprite, x, y);
+                    }
+                }
+                else
+                {
+                    PlaceTile(tileSprite, x, y);
+                }
+
+
+                if (y >= height - 1)
+                {
+                    int t = Random.Range(0, treeChance);
+
+                    if (t == 1)
+                    {
+                        //generate a tree
+                        if (worldTiles.Contains(new Vector2(x, y)))
+                            GenerateTree(x, y + 1);
+                    }
                 }
             }
         }
@@ -56,5 +104,40 @@ public class TerrainGeneration : MonoBehaviour
         }
 
         noiseTexture.Apply();
+    }
+
+    private void GenerateTree(float x, float y)
+    {
+        //define our tree
+
+        //generate log
+        int treeHeight = Random.Range(minTreeHeight, maxTreeHeight);
+        for (int i = 0; i < treeHeight; i++)
+        {
+            PlaceTile(log, x, y + i);
+        }
+
+        //generate leaves
+        PlaceTile(leaf, x, y + treeHeight);
+        PlaceTile(leaf, x, y + treeHeight + 1);
+        PlaceTile(leaf, x, y + treeHeight + 2);
+
+        PlaceTile(leaf, x - 1, y + treeHeight);
+        PlaceTile(leaf, x - 1, y + treeHeight + 1);
+
+        PlaceTile(leaf, x + 1, y + treeHeight);
+        PlaceTile(leaf, x + 1, y + treeHeight + 1);
+    }
+
+    private void PlaceTile(Sprite tileSprite, float x, float y)
+    {
+        GameObject newTile = new GameObject();
+        newTile.transform.parent = this.transform;
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+
+        worldTiles.Add(newTile.transform.position - (Vector3.one * 0.5f));
     }
 }
