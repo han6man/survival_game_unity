b44a974fdf4de957bad535177b64e9e26b4a903d
han6man <han6man@gmail.com>
2025-02-24 18:26:28 +0200

    Character #1

---
+++ UnityProject/Semestr5/Assets/Recurssion/Scripts/MissileSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Recurssion
+{
+    public class MissileSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject missilePrefab;
+        [SerializeField] private int maxSplits = 3;
+        [SerializeField] private float launchForce = 5f; 
+
+        void Start()
+        {
+            SpawnMissiles(transform.position, maxSplits);
+        }
+
+        public void SpawnMissiles(Vector3 position, int remainingSplits)
+        {
+            if (remainingSplits <= 0) return;
+
+            GameObject missile1 = Instantiate(missilePrefab, position, Quaternion.Euler(0, 0, 20));
+            GameObject missile2 = Instantiate(missilePrefab, position, Quaternion.Euler(0, 0, -20));
+            missile1.GetComponent<Missile>().Launch(remainingSplits - 1);
+            missile2.GetComponent<Missile>().Launch(remainingSplits - 1);
+        }
+    }
+}
---
+++ UnityProject/Survival_Game_Unity/Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool onGround;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+            onGround = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+            onGround = false;
+    }
+
+    private void FixedUpdate()
+    {
+        // do stuff
+        float horizontal = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxisRaw("Jump");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        if (horizontal > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (horizontal < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        if (vertical > 0.1f || jump > 0.1f)
+        {
+            if (onGround)
+                movement.y = jumpForce;
+        }
+
+        rb.velocity = movement;
+    }
+}
