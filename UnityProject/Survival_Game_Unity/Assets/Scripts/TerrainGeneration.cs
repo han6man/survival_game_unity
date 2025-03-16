6420a5e7888d03c448376a19924a7abebac467de
han6man <han6man@gmail.com>
2025-03-16 19:42:51 +0200

    Occlusion Culling #11

--- UnityProject/Survival_Game_Unity/Assets/Scripts/TerrainGeneration.cs
+++ UnityProject/Survival_Game_Unity/Assets/Scripts/TerrainGeneration.cs
@@ -59,6 +59,28 @@ public class TerrainGeneration : MonoBehaviour
         mainCamera.worldSize = worldSize;
 
         player.Spawn();
+
+        RefreshChunks();
+    }
+
+    private void Update()
+    {
+        RefreshChunks();
+    }
+
+    private void RefreshChunks()
+    {
+        for (int i = 0; i < worldChunks.Length; i++)
+        {
+            if (Vector2.Distance(new Vector2((i * chunkSize) + (chunkSize / 2), 0), new Vector2(player.transform.position.x, 0)) > Camera.main.orthographicSize * 4f)
+            {
+                worldChunks[i].SetActive(false);
+            }
+            else
+            {
+                worldChunks[i].SetActive(true);
+            }
+        }
     }
 
     private void DrawBiomeMap()
